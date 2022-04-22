72d885d4e6b00cb616fd4545e8872a979eef4ec2
kyojro <rostik.kuharchuk@gmail.com>
2022-04-22 18:42:37 +0300

    final version of project.

--- myfile.sh
+++ myfile.sh
@@ -1,14 +1,24 @@
 
 #!/bin/bash
 
-#WORK_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
-#echo $WORK_DIR
+WORK_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
+echo $WORK_DIR
 
 PATH_DIR="$(grep "path" environments.txt | cut -d " " -f 2)"
-#LOGIN="$(grep "login" environments.txt | cut -d " " -f 2)"
-#PASSWORD="$(grep "password" environments.txt | cut -d " " -f 2)"
-#wget --login="$LOGIN" --password="$PASSWORD" "$PATH" -nd -r -A .json
+wget $PATH_DIR -nd -r -A .json
+#ls -ll
+ARCH_NAME="$(grep "user_agent" ./*.json | cut -d "\"" -f 4 | sed 's/ /_/g')"
+echo "ARCHIVE NAME:$ARCH_NAME"
 
-ARCH_NAME="$(grep "user_agent" ./text.txt | cut -d "\"" -f 4)"
-echo $ARCH_NAME
-#tar cvjf "$ARCH_NAME".tgz *.json
+tar cvjf $ARCH_NAME.tgz *.json
+md5sum $ARCH_NAME.tgz > $ARCH_NAME.md5
+ls -ll
+
+
+git clone git@github.com:kyojro/first_step_bash.git
+mv $ARCH_NAME.tgz $ARCH_NAME.md5 ./first_step_bash/
+cd ~/Desktop/task/first_step_bash/
+git add $ARCH_NAME.tgz $ARCH_NAME.md5
+git commit
+git push origin main
+echo "End."
